5b5f4c2684dab517158031c7f4d42541e2084a9c
Philip Jägenstedt <philip@foolip.org>
2014-09-09 21:45:01 +0200

    Solve problem 20

---
+++ problem20.hs
@@ -0,0 +1,9 @@
+factorial :: (Integral a) => a -> a
+factorial 1 = 1
+factorial n = n * factorial (n - 1)
+
+digitSum :: (Integral a) => a -> a
+digitSum 0 = 0
+digitSum n = (mod n 10) + digitSum (div n 10)
+
+answer = digitSum $ factorial 100
