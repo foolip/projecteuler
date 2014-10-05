6dfd204c88f5999cf9bf04ebb7193f13e4b121f2
Philip Jägenstedt <philip@foolip.org>
2014-10-05 13:16:02 +0200

    Get rid of the caching for problem 44
    The speed improvment is ~14s -> ~ 11s.

--- p044.cc
+++ p044.cc
@@ -9,28 +9,6 @@
 namespace {
 
 template <typename T>
-class Pentagonal {
- public:
-  Pentagonal() : cache_ { 0 } {}
-
-  T operator()(size_t n) {
-    if (cache_.size() <= n) {
-      size_t old_size = cache_.size();
-      assert(old_size > 0);
-      cache_.resize(n + 1);
-      for (size_t i = old_size; i < cache_.size(); ++i) {
-        cache_[i] = cache_[i - 1] + 3*static_cast<T>(i) - 2;
-        assert(cache_[i] > cache_[i - 1]);
-      }
-    }
-    return cache_[n];
-  }
-
- private:
-  std::vector<T> cache_;
-};
-
-template <typename T>
 bool IsPentagonal(T x) {
   // x = (n * (3*n - 1)) / 2 => n = (sqrt(24*x + 1) + 1) / 6, so x is a
   // pentagonal number if (sqrt(24*x + 1) + 1) / 6 is an integer.
@@ -43,17 +21,22 @@ bool IsPentagonal(T x) {
 
 template <typename T>
 T MatchingPentagonalPairDifference() {
-  Pentagonal<T> p;
+  // p(n) is the nth pentagonal number.
+  auto p = [] (T n) {
+    assert(n > 0);
+    return (n * (3*n - 1)) / 2;
+  };
+
   // The difference between p(k) and p(j) will be a pentagonal number, so find
   // for and test pairs where the difference is a pentagonal number.
-  for (size_t n = 1; n < std::numeric_limits<size_t>::max(); ++n) {
+  for (T n = 1; n < std::numeric_limits<T>::max(); ++n) {
     const T diff = p(n);
     // FIXME: Increment j and k with finesse so that the diff_k_j alternates
     // around diff until either it matches or j==k.
-    for (size_t j = 1; ; ++j) {
+    for (T j = 1; ; ++j) {
       if (p(j + 1) - p(j) > diff)
         break;
-      for (size_t k = j + 1; ; ++k) {
+      for (T k = j + 1; ; ++k) {
         const T diff_k_j = p(k) - p(j);
         assert(diff_k_j > 0);
         if (diff_k_j == diff) {
